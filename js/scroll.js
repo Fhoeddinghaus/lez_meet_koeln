b0b3ea85c745b21f56b75bafb8d02e47c6f94987
Lilith Emilia Höddinghaus <info@xefli.de>
2026-02-05 08:24:28 +0100

    updated styles and content

--- js/scroll.js
+++ js/scroll.js
@@ -7,13 +7,15 @@ function scrollFunction() {
     //document.getElementById("headernav").style.height = "10vh";
     //document.getElementById("headerouter").style.height = "12.5vh";
     $("header").addClass("shrunk");
-    document.documentElement.style.setProperty("--header-offset", "10vh");
+    //document.documentElement.style.setProperty("--header-offset", "10vh");
+    document.documentElement.style.setProperty("--header-offset", "var(--header-offset-min)");
   } else if (document.body.scrollTop < 40 || document.documentElement.scrollTop < 40) {
     //document.getElementById("headerlogo").style.height = "18vh";
     //document.getElementById("headernav").style.height = "20vh";
     //document.getElementById("headerouter").style.height = "22.5vh";
     $("header").removeClass("shrunk");
-    document.documentElement.style.setProperty("--header-offset", "20vh");
+    //document.documentElement.style.setProperty("--header-offset", "20vh");
+    document.documentElement.style.setProperty("--header-offset", "var(--header-offset-max)");
   }
 } 
 
