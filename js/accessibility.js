709cfc13efdf5623671f3140e0ccf983c4cbbb41
Lilith Emilia Höddinghaus <info@xefli.de>
2026-02-16 21:54:44 +0100

    fixes

--- js/accessibility.js
+++ js/accessibility.js
@@ -1,6 +1,10 @@
 // high contrast toggle
-document.querySelector(".button-high-contrast").addEventListener("click", function(event) {
-    // prevent default link behavior
-    event.preventDefault();
-    document.body.classList.toggle("high-contrast");
+// add for every button with button-high-contrast class
+
+document.querySelectorAll(".button-high-contrast").forEach(function(button) {
+    button.addEventListener("click", function(event) {
+        // prevent default link behavior
+        event.preventDefault();
+        document.body.classList.toggle("high-contrast");
+    });
 });
